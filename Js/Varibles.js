b4f6d85477644163a50615335c276e2eb91180e6
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-04-09 14:53:22 +0530

    create additon of two number code

---
+++ Js/Varibles.js
@@ -0,0 +1,13 @@
+let p = 10;
+let q = 20;
+var c = addition(10, 45);
+var d = addition(p, q);
+//const sum = "bharat
+console.log("additioin is :" + c)
+console.log("another addition is :" + d)
+
+
+function addition(a, b) {
+    return a + b;
+
+}
