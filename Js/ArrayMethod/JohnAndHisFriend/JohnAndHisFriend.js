a40d19177360c35f7c0030a2771075381c52a044
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-04-17 11:53:03 +0530

    updated code

--- Js/ArrayMethod/JohnAndHisFriend/JohnAndHisFriend.js
+++ Js/ArrayMethod/JohnAndHisFriend/JohnAndHisFriend.js
@@ -1,31 +1,26 @@
 function PrintDeveloperbyMap(employees) {
     const developers = employees.filter(employee => employee.profession === "developer");
-    developers.forEach(dev => console.log(dev));
-    return developers;
+    const developerNames = developers.map(dev => dev.name);
+    console.log(developerNames);
 }
 
 function PrintDeveloperbyForEach(employees) {
-    const developerEmployees = [];
     employees.forEach(employee => {
         if (employee.profession === "developer") {
-            developerEmployees.push(employee);
             console.log(employee);
         }
     });
-    return developerEmployees;
 }
 
 function addData(employees) {
-    const newEmployee = { id: 4, name: "susan", age: "20", profession: "intern" };
+    const newEmployee = { id: 4, name: "Susan", age: "20", profession: "intern" };
     employees.push(newEmployee);
-    console.log(newEmployee);
-    return newEmployee;
+    console.log("Added employee:", newEmployee);
 }
 
 function removeAdmin(employees) {
     const filteredEmployees = employees.filter(employee => employee.profession !== "admin");
-    console.log(filteredEmployees);
-    return filteredEmployees;
+    console.log("Employees after removing admin:", filteredEmployees);
 }
 
 function ConcatinateArray(employees) {
@@ -34,9 +29,9 @@ function ConcatinateArray(employees) {
         { id: 6, name: "Bob", age: "30", profession: "designer" },
         { id: 7, name: "Charlie", age: "22", profession: "developer" }
     ];
+
     const concatenatedArray = employees.concat(newArray);
-    console.log(concatenatedArray);
-    return concatenatedArray;
+    console.log("Concatenated array:", concatenatedArray);
 }
 
 module.exports = { PrintDeveloperbyMap, PrintDeveloperbyForEach, addData, removeAdmin, ConcatinateArray };
