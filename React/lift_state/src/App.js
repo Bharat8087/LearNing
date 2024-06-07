4c8a6e7e78db80c99e57ed108bb009c19f5e286f
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-06-07 10:33:42 +0530

    create lift state up 5

---
+++ React/lift_state/src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import LoginForm from './LoginForm';
+
+const App = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const handleLogin = () => {
+        setIsLoggedIn(true);
+    };
+
+    return ( <
+        div >
+        <
+        h1 > Parent Component < /h1> <
+        h2 > { isLoggedIn ? "Welcome, User!" : "Please Log In" } < /h2> <
+        LoginForm isLoggedIn = { isLoggedIn }
+        onLogin = { handleLogin }
+        /> <
+        /div>
+    );
+};
+
+export default App;
