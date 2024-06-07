406e1fe1a721ef5be51fd983aa9ae275d3512222
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-06-07 10:11:45 +0530

    create react-comp

--- React/conditionweatherreport/src/WeatherDisplay.jsx
+++ React/conditionweatherreport/src/WeatherDisplay.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 const WeatherDisplay = ({ weather }) => {
   const temperatureStyle = {
@@ -8,18 +7,13 @@ const WeatherDisplay = ({ weather }) => {
 
   return (
     <div>
-      <p style={temperatureStyle}>Temperature: {weather.temperature}°C</p>
+      <p style={temperatureStyle}>Temperature: {weather.temperature}</p>
       <span style={temperatureStyle}>Temperature Color</span>
       <p>Conditions: {weather.conditions}</p>
     </div>
   );
 };
 
-WeatherDisplay.propTypes = {
-  weather: PropTypes.shape({
-    temperature: PropTypes.number.isRequired,
-    conditions: PropTypes.string.isRequired,
-  }).isRequired,
-};
+
 
 export default WeatherDisplay;
